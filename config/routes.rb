60f3c3595ddbe4ce740c00394867513a48381130
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-26 14:54:32 +0300

    commit tree e573fff13565a0ea34a42797e8e220fbc285def0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # route for all the students
+  get 'students', to: 'students#index'
+
+  # route for student grades 
+  get 'students/grades', to: 'students#grades'
+
+  #
+
+
 end
